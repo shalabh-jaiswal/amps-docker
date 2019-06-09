222aa83216663e8d917e8a25e6913910a1e5b35b
Shalabh Jaiswal <sjdesk@sj.com>
2019-06-08 21:12:59 -0400

    replaced run.sh with docker-compose.yml

--- amps-entrypoint.sh
+++ amps-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
+cd /amps/AMPS-5.3.0.5-Release-Linux/bin/ 
+
 /amps/AMPS-5.3.0.5-Release-Linux/bin/ampServer --sample-config > ./config.xml
 /amps/AMPS-5.3.0.5-Release-Linux/bin/ampServer ./config.xml
--- run.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run -it --name amps-snapshot-1 -p 8085:8085 -p 9007:9007 jaiswals/amps:latest
